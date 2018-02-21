196f45d3b7122cce599ccae06ebca59347303970
Ksmith30 <kylesmith0630@gmail.com>
2018-02-21 10:17:19 -0600

    Update DetailActivity with TextView

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,44 +1,86 @@
 package com.example.android.sunshine;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class DetailActivity extends AppCompatActivity {
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.utilities.SunshineDateUtils;
+import com.example.android.sunshine.utilities.SunshineWeatherUtils;
+
+import org.w3c.dom.Text;
+
+@RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
+public class DetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
 
     private String mForecast;
-    private TextView mWeatherDisplay;
+    private String[] mWeatherData = {
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_HUMIDITY,
+            WeatherContract.WeatherEntry.COLUMN_WIND_SPEED,
+            WeatherContract.WeatherEntry.COLUMN_PRESSURE,
+            WeatherContract.WeatherEntry.COLUMN_DEGREES
+    };
+
+    private static final int COLUMN_DATE = 1;
+    private static final int COLUMN_WEATHER = 2;
+    private static final int COLUMN_HIGH = 3;
+    private static final int COLUMN_LOW = 4;
+    private static final int COLUMN_HUMIDITY = 5;
+    private static final int COLUMN_WIND = 6;
+    private static final int COLUMN_PRESSURE = 7;
+    private static final int COLUMN_DEGREES = 8;
+
+    private static final int LOADER_ID = 84;
+
+    private Uri mUri;
+
+    private TextView mDate;
+    private TextView mDescription;
+    private TextView mHigh;
+    private TextView mLow;
+    private TextView mHumidity;
+    private TextView mWind;
+    private TextView mPressure;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        mWeatherDisplay = (TextView) findViewById(R.id.tv_display_weather);
+        mDate = (TextView) findViewById(R.id.selected_day_date);
+        mDescription = (TextView) findViewById(R.id.selected_day_weather_description);
+        mHigh = (TextView) findViewById(R.id.selected_day_high_temp);
+        mLow = (TextView) findViewById(R.id.selected_day_low_temp);
+        mHumidity = (TextView) findViewById(R.id.selected_day_humidity);
+        mWind = (TextView) findViewById(R.id.selected_day_wind);
+        mPressure = (TextView) findViewById(R.id.selected_day_pressure);
 
         Intent intentThatStartedThisActivity = getIntent();
-
-        if (intentThatStartedThisActivity != null) {
-            if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
-                mForecast = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
-                mWeatherDisplay.setText(mForecast);
-            }
+        mUri = intentThatStartedThisActivity.getData();
+        if (mUri == null) {
+            throw new NullPointerException("There is no Uri in the intent!");
         }
+
+        getSupportLoaderManager().initLoader(LOADER_ID, null, this);
     }
 
-    /**
-     * Uses the ShareCompat Intent builder to create our Forecast intent for sharing. We set the
-     * type of content that we are sharing (just regular text), the text itself, and we return the
-     * newly created Intent.
-     *
-     * @return The Intent to use to start our share.
-     */
     private Intent createShareForecastIntent() {
         Intent shareIntent = ShareCompat.IntentBuilder.from(this)
                 .setType("text/plain")
@@ -66,5 +108,71 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        switch (i) {
+            case LOADER_ID:
+
+                return new CursorLoader(this,
+                        mUri,
+                        mWeatherData,
+                        null,
+                        null,
+                        null);
+            default:
+                throw new RuntimeException("Loader Not Implemented" + LOADER_ID);
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        boolean cursorHasValidData = false;
+        if (data != null && data.moveToFirst()) {
+            cursorHasValidData = true;
+        } if (!cursorHasValidData) {
+            return;
+        }
+
+        long localDateMidnightGmt = data.getLong(COLUMN_DATE);
+        String dateText = SunshineDateUtils.getFriendlyDateString(this, localDateMidnightGmt,
+                true);
+        mDate.setText(dateText);
+
+        int weatherID = data.getInt(COLUMN_WEATHER);
+        String weatherDescription = SunshineWeatherUtils.getStringForWeatherCondition(this, weatherID);
+        mDescription.setText(weatherDescription);
+
+        double highTemp = data.getDouble(COLUMN_HIGH);
+        String highString= SunshineWeatherUtils.formatTemperature(this, highTemp);
+        mHigh.setText(highString);
+
+        double lowTemp = data.getDouble(COLUMN_LOW);
+        String lowString = SunshineWeatherUtils.formatTemperature(this, lowTemp);
+        mLow.setText(lowString);
+
+        float humidity = data.getFloat(COLUMN_HUMIDITY);
+        String humidityString = getString(R.string.format_humidity, humidity);
+        mHumidity.setText(humidityString);
+
+        float windSpeed = data.getFloat(COLUMN_WIND);
+        float windDirection = data.getFloat(COLUMN_DEGREES);
+        String wind = SunshineWeatherUtils.getFormattedWind(this, windSpeed, windDirection);
+        mWind.setText(wind);
+
+        float pressure = data.getFloat(COLUMN_PRESSURE);
+        String pressureString = getString(R.string.format_pressure, pressure);
+        mPressure.setText(pressureString);
+
+        mForecast = String.format("%s - %s - %s/%s",
+                dateText, weatherDescription, highString, lowString);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
