5023c3104f0587aaeffb32d7678cc91711bd6e03
Ksmith30 <kylesmith0630@gmail.com>
2018-03-01 09:58:19 -0600

    Add scheduleWeatherSync method and update Manifest

--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -8,15 +8,50 @@ import android.net.Uri;
 import android.os.AsyncTask;
 
 import com.example.android.sunshine.data.WeatherContract;
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.RetryStrategy;
+import com.firebase.jobdispatcher.Trigger;
+
+import java.util.concurrent.TimeUnit;
 
 public class SunshineSyncUtils {
 
+    private static final int HOURS_TO_SYNC = 4;
+    private static final int SECONDS_TO_SYNC = (int) TimeUnit.HOURS.toSeconds(HOURS_TO_SYNC);
+    private static final int FLEXTIME_SECONDS = SECONDS_TO_SYNC / 3;
+
     private static boolean sInitialized;
+
+    private static final String SYNC_JOB = "sync-job";
+
+     private static void scheduleWeatherSync(final Context context) {
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(context));
+
+        Job weatherSyncJob = dispatcher.newJobBuilder()
+                .setService(SunshineFirebaseJobService.class)
+                .setTag(SYNC_JOB)
+                .setRecurring(true)
+                .setLifetime(Lifetime.FOREVER)
+                .setConstraints(Constraint.ON_ANY_NETWORK)
+                .setTrigger(Trigger.executionWindow(SECONDS_TO_SYNC, SECONDS_TO_SYNC + FLEXTIME_SECONDS))
+                .setReplaceCurrent(true)
+                .setRetryStrategy(RetryStrategy.DEFAULT_EXPONENTIAL)
+                .build();
+
+        dispatcher.schedule(weatherSyncJob);
+    }
 
     @SuppressLint("StaticFieldLeak")
     synchronized public static void initialize(final Context context) {
 
         if (sInitialized) return;
+        sInitialized = true;
+
+        scheduleWeatherSync(context);
 
         if (!sInitialized) {
             new AsyncTask<Void, Void, Void>() {
@@ -43,7 +78,7 @@ public class SunshineSyncUtils {
                 }
             }.execute();
 
-            sInitialized = true;
+
         }
     }
 
